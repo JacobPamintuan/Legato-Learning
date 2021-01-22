e4018992ad3c72006368517e3bd86985560599d6
JacobPamintuan <jcpamintu@gmail.com>
2021-01-22 00:18:19 -0500

    Comments

--- src/legatoLearning/LegatoLearningTest.java
+++ src/legatoLearning/LegatoLearningTest.java
@@ -1,5 +1,31 @@
 package legatoLearning;
 
+/*
+ * Name: Jacob Pamintuan
+ * 
+ * Date: Friday, January 22nd, 2021
+ * 
+ * Course Code: ICS4U1-01; Mr. Fernandes
+ * 
+ * Title: Legato Learning
+ * 
+ * Description:
+ * 
+ * Features:
+ * 		- Hover states
+ * 		-  
+ * 
+ * Major Skills:
+ * 		- 
+ * 
+ * Areas of Concern
+ * 		- Only runs on Apple Macbooks
+ * 		- Screen size of at least 1440x810
+ * 		- Wait at least 2 seconds after home screen pops up 
+ * 				to allow the program to load
+ * 
+ */
+
 public class LegatoLearningTest {
 
 	public static void main(String[] args) {
@@ -10,10 +36,11 @@ public class LegatoLearningTest {
 
 		try {
 			// Initialize / Load Users
-			new LoadUsers(); 
-			
+			new LoadUsers();
+
 			// Run application
 			new Initialize();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
